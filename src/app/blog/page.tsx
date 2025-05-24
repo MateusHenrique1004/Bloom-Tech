c58d58ff7982612fed23fe352010f57afcbe3ed8
Pedro <pedrohjs_vargas@hotmail.com>
2025-05-24 00:23:40 -0300

    feat:card about concluido e uso de lenis smooth

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,60 @@
+"use client";
 import styles from "./styles.module.scss";
 import { people } from "../../components/Card/data";
 import CardAbout from "@/components/Card/about";
-
+import { useScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Lenis from "@studio-freight/lenis";
 export default function Blog() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
+  //  useEffect(() => {
+  //   const lenis = new Lenis({
+  //     lerp: 0.1,
+  //     smoothWheel: true,
+  //   });
+
+  //   function raf(time) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
+
+  //   const rafId = requestAnimationFrame(raf);
+
+  //   return () => {
+  //     cancelAnimationFrame(rafId);
+  //     lenis.destroy();
+  //   };
+  // }, []);
+
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
   return (
-    <main className={styles.main}>
+    <main ref={container} className={styles.main}>
       {people.map((project, index) => {
-        return <CardAbout key={index} {...project} />;
+        const targetScale = 1 - (people.length - index) * 0.05;
+        return (
+          <CardAbout
+            key={index}
+            i={index}
+            {...project}
+            range={[index * 0.25, 1]}
+            progress={scrollYProgress}
+            targetScale={targetScale}
+          />
+        );
       })}
     </main>
   );
--- src/components/Card/about.tsx
+++ src/components/Card/about.tsx
@@ -3,22 +3,39 @@ import Image from "next/image";
 import styles from "./styles.module.scss";
 import { useScroll, motion, useTransform } from "framer-motion";
 import { useRef } from "react";
-export default function CardAbout({ name, bio, src, color }) {
+export default function CardAbout({
+  i,
+  name,
+  bio,
+  src,
+  color,
+  progress,
+  range,
+  targetScale,
+}) {
   const container = useRef(null);
 
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ["start end", "start start"],
   });
-  const scale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+ 
+
+  const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+
+ const  scale= useTransform(progress, range, [1, targetScale]);
+
 
   return (
     <div ref={container} className={styles.cardContainer}>
-      <div style={{ backgroundColor: color }} className={styles.card}>
-        <motion.div style={{ scale }}>
+      <motion.div
+        style={{scale, backgroundColor: color, top: `calc(-10% + ${i * 25}px)` }}
+        className={styles.card}
+      >
+        <motion.div style={{ scale: imageScale }}>
           <Image src={"/fundo2.jpg"} alt="" width={100} height={100} />
         </motion.div>
-      </div>
+      </motion.div>
     </div>
   );
 }
